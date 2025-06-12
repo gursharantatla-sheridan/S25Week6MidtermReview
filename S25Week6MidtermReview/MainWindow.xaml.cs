cbecc74059d2f182adf86f04324264a93f570f06
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-06-12 14:56:04 -0400

    calculate button

--- S25Week6MidtermReview/MainWindow.xaml.cs
+++ S25Week6MidtermReview/MainWindow.xaml.cs
@@ -16,9 +16,66 @@ namespace S25Week6MidtermReview
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Employee _emp;
+        private List<Employee> _employees;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            rdoHourly.IsChecked = true;
+            _employees = new List<Employee>();
+        }
+
+        private void rdoHourly_Checked(object sender, RoutedEventArgs e)
+        {
+            lblInput2.Content = "Hours Worked:";
+            lblInput3.Content = "Hourly Wage:";
+        }
+
+        private void rdoCommission_Checked(object sender, RoutedEventArgs e)
+        {
+            lblInput2.Content = "Gross Sales:";
+            lblInput3.Content = "Commission Rate:";
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            rdoHourly.IsChecked = true;
+            txtName.Text = txtInput2.Text = txtInput3.Text = "";
+            txtGrossEarnings.Text = txtTax.Text = txtNetEarnings.Text = "";
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+
+            if (rdoHourly.IsChecked == true)
+            {
+                int hours = int.Parse(txtInput2.Text);
+                double wage = double.Parse(txtInput3.Text);
+
+                _emp = new HourlyEmployee(name, hours, wage);
+            }
+            else
+            {
+                double grossSales = double.Parse(txtInput2.Text);
+                double commissionRate = double.Parse(txtInput3.Text) / 100;
+
+                _emp = new CommissionEmployee(name, grossSales, commissionRate);
+            }
+
+            txtGrossEarnings.Text = _emp.GrossEarnings().ToString("C");
+            txtTax.Text = _emp.Tax().ToString("C");
+            txtNetEarnings.Text = _emp.NetEarnings().ToString("C");
+
+            lstEmployees.Items.Add(name);
+            _employees.Add(_emp);
         }
     }
 }
